ddcbdef2d9bf48d89b42f23716396b3528a06b7d
SHILIN <shilinagre006@gmail.com>
2021-03-31 14:20:15 +0530

    practical/

--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from user.forms import UserAdminCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.views import View
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+def register(req):
+    form = UserAdminCreationForm()
+    if req.method == 'POST':
+        form = UserAdminCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(req, 'register.html', {'form': form})
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    success_url = reverse_lazy('register')
+    form_class = UserAdminCreationForm
+    def post(self, request, *args, **kwargs):
+        form = UserAdminCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            ip = request.META.get('REMOTE_ADDR')
+            user.location = ip
+            user.save()
+        return render(request, self.template_name, {'form': form})
+
