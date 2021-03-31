7d8682bb1e4cf654dc3487541913bd48b3aa2ba2
SHILIN <shilinagre006@gmail.com>
2021-03-31 14:20:58 +0530

    ADDED register view to register user

--- practical/urls.py
+++ practical/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from user.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+
+    path('users/', include('django.contrib.auth.urls')),
 ]
